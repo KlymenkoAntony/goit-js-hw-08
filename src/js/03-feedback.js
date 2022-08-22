60aa9050d0aaf5d14a1e4e7ec7582e4d974c9c9e
KlymenkoAntony <102675462+KlymenkoAntony@users.noreply.github.com>
2022-08-22 20:42:42 +0300

    1150 спроба все зробити

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name = "email"]');
+const text = document.querySelector('[name = "message"]');
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function onFormInput() {
+    const object = { email: email.value, message: text.value }; 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(object));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log({ email: email.value, message: text.value });
+    localStorage.removeItem(STORAGE_KEY);
+    evt.currentTarget.reset();
+}
+
+const SavedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (SavedForm) {
+        email.value = SavedForm.email;
+        text.value = SavedForm.message;
+    }
