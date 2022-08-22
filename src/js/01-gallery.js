1d9fb3afd8aed4f65626a7159287cfa26851312b
KlymenkoAntony <102675462+KlymenkoAntony@users.noreply.github.com>
2022-08-22 20:19:59 +0300

    1 tsk

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox'
+import 'simplelightbox/dist/simple-lightbox.min.css'
+import { galleryItems } from './gallery-items'
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const gallery = document.querySelector('.gallery')
+const items = []
+
+galleryItems.forEach((element) => {
+  const galleryList = document.createElement('li')
+  galleryList.className = 'gallerry__item'
+  const galleryLink = document.createElement('a')
+  galleryLink.className = 'gallery__link'
+  galleryLink.href = element.original
+  const galleryImage = document.createElement('img')
+  galleryImage.className = 'gallery__image'
+  galleryImage.src = element.preview
+  galleryImage.setAttribute('title', element.description)
+  galleryImage.alt = element.description
+
+  galleryList.append(galleryLink)
+  galleryLink.append(galleryImage)
+  items.push(galleryList)
+})
+gallery.append(...items)
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionData: 'alt',
+})
